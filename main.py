6114824b53ba5ece222b87125ba3699148195082
St0rm-85 <82373787+St0rm-85@users.noreply.github.com>
2021-06-09 16:50:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,102 @@
-import boto3
-from boto3 import client
+import logging
+import yaml
+import os
+import json
+import sys
 
-# ruleid:hardcoded-token
-client("s3", aws_secret_access_key="jWnyxxxxxxxxxxxxxxxxX7ZQxxxxxxxxxxxxxxxx")
+from collections import defaultdict
+from typing import Dict, List, Set, Any
 
-# ruleid:hardcoded-token
-boto3.sessions.Session(aws_secret_access_key="jWnyxxxxxxxxxxxxxxxxX7ZQxxxxxxxxxxxxxxxx")
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(stream=sys.stderr)
+handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(handler)
 
-s = boto3.sessions
-# ruleid:hardcoded-token
-s.Session(aws_access_key_id="AKIAxxxxxxxxxxxxxxxx")
-
-uhoh_key = "AKIAxxxxxxxxxxxxxxxx"
-ok_secret = os.environ.get("SECRET_ACCESS_KEY")
-# ruleid:hardcoded-token
-s3 = boto3.resource(
-    "s3",
-    aws_access_key_id=uhoh_key,
-    aws_secret_access_key=ok_secret,
-    region_name="sfo2",
-    endpoint_url="https://sfo2.digitaloceanspaces.com",
+LANGS = (
+    "python",
+    "javascript",
+    "java",
+    "go",
 )
 
-ok_key = os.environ.get("ACCESS_KEY_ID")
 
-uhoh_secret = "jWnyxxxxxxxxxxxxxxxxX7ZQxxxxxxxxxxxxxxxx"
-# ruleid:hardcoded-token
-s3 = boto3.resource(
-    "s3",
-    aws_access_key_id=ok_key,
-    aws_secret_access_key=uhoh_secret,
-    region_name="sfo2",
-    endpoint_url="https://sfo2.digitaloceanspaces.com",
-)
+def get_owasp(rule: Dict[str, Any]) -> str:
+    return rule.get("metadata", {}).get("owasp", "")
 
-# ok:hardcoded-token
-s3 = client("s3", aws_access_key_id="this-is-not-a-key")
+def get_cwe(rule: Dict[str, Any]) -> str:
+    return rule.get("metadata", {}).get("cwe", "")
 
-# ok:hardcoded-token
-s3 = boto3.resource(
-    "s3",
-    aws_access_key_id="XXXXXXXX",
-    aws_secret_access_key="----------------",
-    region_name="us-east-1",
-)
+def get_lang(path: str) -> str:
+    for lang in LANGS:
+        if lang in path:
+            return lang
+    return ""
 
-# ok:hardcoded-token
-s3 = boto3.resource(
-    "s3",
-    aws_access_key_id="<your token here>",
-    aws_secret_access_key="<your secret here>",
-    region_name="us-east-1",
-)
+def get_framework(path: str) -> str:
+    # get the dir name immediately under the language
+    for lang in LANGS:
+        if lang in path:
+            s = path.split(os.path.sep)
+            logger.debug(f"Split path={s}")
+            return s[s.index(lang)+1]
+    return ""
 
-# ok:hardcoded-token
-key = os.environ.get("ACCESS_KEY_ID")
-secret = os.environ.get("SECRET_ACCESS_KEY")
-s3 = boto3.resource(
-    "s3",
-    aws_access_key_id=key,
-    aws_secret_access_key=secret,
-    region_name="sfo2",
-    endpoint_url="https://sfo2.digitaloceanspaces.com",
-)
+def is_security(path: str) -> bool:
+    return "security" in path
+
+def is_rule(path: str) -> bool:
+    _, ext = os.path.splitext(path)
+    return "yaml" in ext 
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    # Add arguments here
+    parser.add_argument("directory")
+
+    args = parser.parse_args()
+
+    owasp_matrix = defaultdict(list)
+    cwe_matrix = defaultdict(list)
+    owasp_by_lang_matrix = defaultdict(lambda: defaultdict(list))
+    cwe_by_lang_matrix = defaultdict(lambda: defaultdict(list))
+    owasp_by_framework_matrix = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+    cwe_by_framework_matrix = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+    for dirpath, dirnames, filenames in os.walk(args.directory):
+        for filename in filenames:
+            path = os.path.join(dirpath, filename)
+            if not is_rule(path):
+                continue
+            if not is_security(path):
+                continue
+            logger.debug(f"Opening {path}")
+            with open(path, 'r') as fin:
+                rules = yaml.safe_load(fin)
+                for rule in rules.get('rules', []):
+                    lang = get_lang(path)
+                    framework = get_framework(path)
+                    cwe = get_cwe(rule)
+                    owasp = get_owasp(rule)
+                    owasp_matrix[owasp].append((path, rule))
+                    cwe_matrix[cwe].append((path, rule))
+                    owasp_by_lang_matrix[owasp][lang].append((path, rule))
+                    cwe_by_lang_matrix[cwe][lang].append((path, rule))
+                    owasp_by_framework_matrix[owasp][lang][framework].append((path, rule))
+                    cwe_by_framework_matrix[cwe][lang][framework].append((path, rule))
+
+    print(json.dumps({
+        "owasp": {
+            "totals": {owasp: len(v) for owasp, v in sorted(owasp_matrix.items())},
+            #"per_language": {owasp: {lang: len(v) for lang, v in owasp_by_lang_matrix[owasp].items()} for owasp in sorted(owasp_by_lang_matrix)},
+            "per_framework": {owasp: {lang: {frm: len(v) for frm, v in owasp_by_framework_matrix[owasp][lang].items()} for lang in sorted(owasp_by_framework_matrix[owasp])} for owasp in sorted(owasp_by_framework_matrix)},
+            "rules_with_no_owasp": [t[0] for t in owasp_matrix[""]],
+        },
+        "cwe": {
+            "totals": {cwe: len(v) for k, v in sorted(cwe_matrix.items())},
+            #"per_language": {cwe: {lang: len(v) for lang, v in cwe_by_lang_matrix[cwe].items()} for cwe in sorted(cwe_by_lang_matrix)},
+            "per_framework": {cwe: {lang: {frm: len(v) for frm, v in cwe_by_framework_matrix[cwe][lang].items()} for lang in sorted(cwe_by_framework_matrix[cwe])} for cwe in sorted(cwe_by_framework_matrix)},
+            "rules_with_no_cwe": [t[0] for t in cwe_matrix[""]],
+        }
+    }))
