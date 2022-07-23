82f8aadc67e72428014cf417fd6caed418108041
St0rm85 <82373787+St0rm-85@users.noreply.github.com>
2022-07-23 13:19:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -30,3 +30,4 @@ def upload_and_unpack(c):
     if c.run('test -f /opt/mydata/myfile', warn=True).failed:
         c.put('myfiles.tgz', '/opt/mydata')
         c.run('tar -C /opt/mydata -xzvf /opt/mydata/myfiles.tgz')
+        
